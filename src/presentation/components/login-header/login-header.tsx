f14c08b81187aea14a8300a1d33712fa0f7ed380
Hugo C. Durães <hugocalheira@gmail.com>
2022-03-22 11:33:37 -0300

    fix: login-header - disable eslint for a specific line

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -5,6 +5,7 @@ import Styles from './login-header-styles.scss'
 type Props = React.HTMLAttributes<HTMLElement>
 
 const LoginHeader: React.FC<Props> = (props: Props) => (
+    // eslint-disable-next-line react/prop-types
     <header {...props} className={[Styles.header, props.className].join(' ')}>
         <Logo />
         <h1>4Dev - Enquetes para Programadores</h1>
