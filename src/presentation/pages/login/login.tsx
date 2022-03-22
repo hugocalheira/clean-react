48f53bf4b4948dc087b8c2de8f7c7d870c9ddfda
Hugo C. Durães <hugocalheira@gmail.com>
2022-03-22 09:41:58 -0300

    refactor: move FormStatus to his own component

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Spinner from '@/presentation/components/spinner/spinner'
 import Header from '@/presentation/components/login-header/login-header'
 import Input from '@/presentation/components/input/input'
 import Footer from '@/presentation/components/footer/footer'
 import Styles from './login-styles.scss'
+import FormStatus from '@/presentation/components/form-status/form-status'
 
 const Login: React.FC = () => {
   return (
@@ -17,11 +17,7 @@ const Login: React.FC = () => {
             <button type='submit'>Entrar</button>
 
             <span className={Styles.link}>Criar conta</span>
-
-            <div className={Styles.errorWrap}>
-                <Spinner className={Styles.spinner}/>
-                <span className={Styles.error}>Erro</span>
-            </div>
+            <FormStatus />
 
         </form>
         <Footer />
