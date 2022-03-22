68cfc5c71beeda8d3a3b520ae3e920aece6d7473
Hugo C. Durães <hugocalheira@gmail.com>
2022-03-22 01:08:05 -0300

    refactor: move Header to his own file

---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+import Logo from '../logo/logo'
+import Styles from './login-header-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+const LoginHeader: React.FC<Props> = (props: Props) => (
+    <header {...props} className={[Styles.header, props.className].join(' ')}>
+        <Logo />
+        <h1>4Dev - Enquetes para Programadores</h1>
+    </header>
+)
+
+export default memo(LoginHeader)
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import Logo from '@/presentation/components/logo/logo'
+import Header from '@/presentation/components/login-header/login-header'
 import Spinner from '@/presentation/components/spinner/spinner'
 import React from 'react'
 import Styles from './login-styles.scss'
@@ -6,10 +6,7 @@ import Styles from './login-styles.scss'
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-        <header className={Styles.header}>
-            <Logo />
-            <h1>4Dev - Enquetes para Programadores</h1>
-        </header>
+        <Header />
         <form className={Styles.form}>
             <h2>Login</h2>
             <div className={Styles.inputWrap}>
