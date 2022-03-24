877e8211ee10cad6d27b32cf53cde538e421faa2
Hugo C. Durães <hugocalheira@gmail.com>
2022-03-24 19:10:57 -0300

    feat: ensure Login calls Validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,11 +5,17 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
 
   function enableInput (event: React.FocusEvent<HTMLInputElement>): void {
     event.target.readOnly = false
+  }
+
+  function handleChange (e: React.FocusEvent<HTMLInputElement>): void {
+    setState(oldState => (
+      { ...oldState, [props.name]: e.target.value }
+    ))
   }
 
   const getTitle = (): string => error
@@ -18,7 +24,14 @@ const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles.inputWrap}>
-        <input {...props} autoComplete='off' readOnly onFocus={enableInput}/>
+        <input
+          {...props}
+          autoComplete='off'
+          readOnly
+          onFocus={enableInput}
+          onChange={handleChange}
+          value={state[props.name]}
+        />
         <span
           title={getTitle()}
           data-testid={`${props.name}-status`}
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,23 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header, Input, Footer, FormStatus } from '@/presentation/components'
 import Styles from './login-styles.scss'
 import Context from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
-const Login: React.FC = () => {
-  const [state] = useState({
-    isLoading: false
+type Props = {
+  validation: Validation
+}
+
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
+    email: '',
+    isLoading: false,
+    mainError: '',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório'
   })
 
-  const [errorState] = useState({
-    main: '',
-    email: 'Campo obrigatório',
-    password: 'Campo obrigatório'
-  })
+  useEffect(() => {
+    const { email } = state
+    validation.validate({ email })
+  }, [state.email])
 
   return (
     <div className={Styles.login}>
         <Header />
-        <Context.Provider value={{ state, errorState }}>
+        <Context.Provider value={{ state, setState }}>
           <form className={Styles.form}>
               <h2>Login</h2>
 
