3b4f3043d1f72ba69c199304246845e44d262b4d
Hugo C. Durães <hugocalheira@gmail.com>
2022-03-27 15:21:40 -0300

    feat: ensure not call Authentication if form is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -36,8 +36,8 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   async function handleSubmit (event: React.FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault()
-    const { email, password, isLoading } = state
-    if (isLoading) {
+    const { email, password, isLoading, emailError, passwordError } = state
+    if (isLoading || emailError || passwordError) {
       return
     }
     setState({ ...state, isLoading: true })
@@ -48,7 +48,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     <div className={Styles.login}>
         <Header />
         <Context.Provider value={{ state, setState }}>
-          <form className={Styles.form} onSubmit={handleSubmit}>
+          <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
               <h2>Login</h2>
 
               <Input type='email' name='email' placeholder='Digite seu e-mail'/>
