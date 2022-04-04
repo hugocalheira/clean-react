ed44d76d19c6ac377fae33479440e8cfc03129c9
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-04 17:18:50 -0300

    refactor: use faker to mock test data

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,18 +1,21 @@
 import { InvalidFieldError } from '@/validation/errors'
+import faker from '@faker-js/faker'
 import { MinLengthValidation } from './min-length-validation'
 
-const makeSut = (): MinLengthValidation => new MinLengthValidation('field', 8)
+const MIN_LENGTH = 8
+const INVALID_LENGTH = MIN_LENGTH - 1
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), MIN_LENGTH)
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('12345')
+    const error = sut.validate(faker.random.alphaNumeric(INVALID_LENGTH))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
     const sut = makeSut()
-    const error = sut.validate('12345678')
+    const error = sut.validate(faker.random.alphaNumeric(MIN_LENGTH))
     expect(error).toBeFalsy()
   })
 })
