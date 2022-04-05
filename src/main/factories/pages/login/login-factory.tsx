44ad19dd6edc30b0f489981c54cb59cef245228f
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-05 19:55:32 -0300

    refactor: move AxiosHttpClient to a factory method

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios-http-client'
 import { ValidationComposite } from '@/validation/validators'
 import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+import { makeAxiosHttpClient } from '../../http/axios-http-client-factory'
 
 export const makeLogin: React.FC = () => {
   const url = 'http://fordevs.herokuapp.com/api/login'
-  const axiosHttpClient = new AxiosHttpClient()
-  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
+  const remoteAuthentication = new RemoteAuthentication(url, makeAxiosHttpClient())
 
   const validationComposite = ValidationComposite.build([
     ...ValidationBuilder.field('email').required().email().build(),
