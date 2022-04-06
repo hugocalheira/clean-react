c3b336dcba0412bcff157e2822b0019860348a0b
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-06 14:00:08 -0300

    test: ensure to Input Component remove readOnly property on focus

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
 import faker from '@faker-js/faker'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
@@ -18,5 +18,13 @@ describe('Input Component', ()=>{
         const sut = makeSut({fieldName})
         const input = sut.getByTestId(fieldName) as HTMLInputElement
         expect(input.readOnly).toBe(true)
+    })
+    
+    test('Should remove readOnly property on focus', () => {
+        const fieldName = faker.random.word()
+        const sut = makeSut({fieldName})
+        const input = sut.getByTestId(fieldName) as HTMLInputElement
+        fireEvent.focus(input)
+        expect(input.readOnly).toBe(false)
     })
 })
