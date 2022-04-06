75041e7fdb443ac30bbcf0a41bb129f55bd1b546
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-06 13:54:22 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
+import faker from '@faker-js/faker'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
 
+const makeSut = ({fieldName}: {fieldName:string}): RenderResult => {
+    return render(
+        <Context.Provider value={{state:{}}}>
+            <Input name={fieldName}/>
+        </Context.Provider>
+    )
+}
+
 describe('Input Component', ()=>{
     test('Should begin with readOnly property', () => {
-        const { getByTestId } = render(
-            <Context.Provider value={{state:{}}}>
-                <Input name='any_field'/>
-            </Context.Provider>
-        )
-        const input = getByTestId('any_field') as HTMLInputElement
+        const fieldName = faker.random.word()
+        const sut = makeSut({fieldName})
+        const input = sut.getByTestId(fieldName) as HTMLInputElement
         expect(input.readOnly).toBe(true)
     })
 })
