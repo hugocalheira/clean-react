0594c46286daaa30e83c9052957e9e2dba685828
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-11 23:36:34 -0300

    refactor: rename data mocks

---
+++ src/data/test/mock-http.ts
@@ -0,0 +1,21 @@
+import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '../protocols/http'
+import faker from '@faker-js/faker'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.words()
+})
+
+export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
+  url?: string
+  body?: T
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async post ({ url, body }: HttpPostParams<T>): Promise<HttpResponse<R>> {
+    this.url = url
+    this.body = body
+    return await Promise.resolve(this.response)
+  }
+}
