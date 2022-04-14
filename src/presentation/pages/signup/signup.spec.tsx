87886c696b0124dfa5e88f4805991dd33c5cbefa
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-14 15:46:08 -0300

    refactor: change spy method from mockReturnValueOnce to mockRejectedValueOnce

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -152,7 +152,7 @@ describe('SignUp Component', () => {
   test('Should present error if AddAccount fails', async () => {
     const { sut, addAccountSpy } = MakeSut()
     const error = new EmailInUseError()
-    jest.spyOn(addAccountSpy, 'add').mockReturnValueOnce(Promise.reject(error))
+    jest.spyOn(addAccountSpy, 'add').mockRejectedValueOnce(error)
     await simulateValidSubmit(sut)
     await Helper.testElementText(sut, 'main-error', error.message)
     Helper.testChildCount(sut, 'errorWrap', 1)
