76ebf86391b88c1392a3a87923f3f213ac196513
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-20 12:10:37 -0300

    chore: add cypress configuration

---
+++ src/main/test/cypress/plugins/cy-ts-preprocessor.js
@@ -0,0 +1,23 @@
+const wp = require('@cypress/webpack-preprocessor')
+
+module.exports = wp({
+    webpackOptions: {
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: [/node_modules/],
+                    loader: 'ts-loader'
+                    // use: [
+                    //     {
+                    //         loader: 'ts-loader'
+                    //     }
+                    // ]
+                }
+            ]
+        }
+    }
+})
