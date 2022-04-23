5914779663d86a937bc96c3ea4808af57a0e8a59
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-23 17:55:16 -0300

    refactor: move Icon to its own file

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Header } from './header/header'
 export { default as LoginHeader } from './login-header/login-header'
 export { default as Logo } from './logo/logo'
+export { default as Icon, IconName } from './icon/icon'
 export { default as Input } from './input/input'
 export { default as Spinner } from './spinner/spinner'
 export { default as FormStatus } from './form-status/form-status'
