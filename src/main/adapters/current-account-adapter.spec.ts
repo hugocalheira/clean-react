230c724f50ed8bf0600d823e009a659e7fe546e4
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-24 18:29:49 -0300

    test: add GetCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,19 +1,27 @@
 import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
-import { setCurrentAccountAdapter } from './current-account-adapter'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from './current-account-adapter'
 
 jest.mock('@/infra/cache/local-storage-adapter')
 
 describe('CurrentAccountAdapter', () => {
-  test('Should call LocalStorageAdapter with correct values', () => {
+  test('Should call LocalStorageAdapter.set with correct values', () => {
     const account = mockAccountModel()
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
   })
 
-  test('Should throw UnexpectedError if LocalStorageAdapter receive invalid params', () => {
+  test('Should throw UnexpectedError if LocalStorageAdapter.set receive invalid params', () => {
     expect(() => setCurrentAccountAdapter(undefined)).toThrowError(UnexpectedError)
+  })
+
+  test('Should call LocalStorageAdapter.get with correct value', () => {
+    const mockedAccount = mockAccountModel()
+    const getSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce(mockedAccount)
+    const currentAccount = getCurrentAccountAdapter()
+    expect(currentAccount).toEqual(mockedAccount)
+    expect(getSpy).toHaveBeenCalledWith('account')
   })
 })
