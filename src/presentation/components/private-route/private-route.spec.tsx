a8d3b0fe762a09f55b11a782ee4b90eef42f91c6
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-25 12:41:55 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,7 +1,11 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { PrivateRoute } from '@/presentation/components'
 import { BrowserRouter } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
+import faker from '@faker-js/faker'
 
 // pay attention to write it at the top level of your file
 const mockedUsedNavigate = jest.fn()
@@ -11,17 +15,31 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate
 }))
 
-const makeSut = (): void => {
+const makeSut = (account: AccountModel = mockAccountModel()): {textContent: string, componentId: string} => {
+  const getCurrentAccountMock = jest.fn().mockReturnValueOnce(account)
+  const textContent = faker.random.word()
+  const componentId = faker.datatype.uuid()
   render(
-    <BrowserRouter >
-      <PrivateRoute />
-    </BrowserRouter>
+    <ApiContext.Provider value={{ getCurrentAccount: getCurrentAccountMock }}>
+      <BrowserRouter >
+        <PrivateRoute component={() => <div data-testid={componentId}>{textContent}</div>}/>
+      </BrowserRouter>
+    </ApiContext.Provider>
   )
+  return { textContent, componentId }
 }
 
 describe('PrivateRoute', () => {
+  afterEach(jest.clearAllMocks)
   test('Should redirect to /login if token is empty', () => {
-    makeSut()
+    makeSut(null)
     expect(mockedUsedNavigate).toHaveBeenCalledWith('/login', { replace: true })
+  })
+
+  test('Should render current component if token is not empty', () => {
+    const { componentId, textContent } = makeSut()
+    const Element = screen.getByTestId(componentId)
+    expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/login', { replace: true })
+    expect(Element.textContent).toBe(textContent)
   })
 })
