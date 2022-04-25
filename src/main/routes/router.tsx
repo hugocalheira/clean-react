ba616bc6c631543117a75eaafe945bf80853880c
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-25 12:41:48 -0300

    feat: ensure PrivateRoute renders component if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { makeLogin as MakeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp as MakeSignUp } from '@/main/factories/pages/signup/signup-factory'
-import { setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 import { ApiContext } from '@/presentation/contexts'
 import { SurveyList } from '@/presentation/pages'
+import { PrivateRoute } from '@/presentation/components'
 
 const Router: React.FC = () => {
   return (
     <ApiContext.Provider value={{
-      setCurrentAccount: setCurrentAccountAdapter
+      setCurrentAccount: setCurrentAccountAdapter,
+      getCurrentAccount: getCurrentAccountAdapter
     }}>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<MakeLogin />} />
           <Route path="/signup" element={<MakeSignUp />} />
-          <Route path="/" element={<SurveyList />} />
+          <Route path="/" element={<PrivateRoute component={SurveyList} />} />
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { useNavigate, RouteProps } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const privatRoute: React.FC<RouteProps> = (props: RouteProps) => {
+type PrivateProps = {
+  component: React.FC
+}
+// const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+const PrivateRoute: React.FC<PrivateProps> = ({ component: Component }: PrivateProps) => {
   const navigate = useNavigate()
-  navigate('/login', { replace: true })
-  return null
+  const { getCurrentAccount } = useContext(ApiContext)
+  const accessToken = getCurrentAccount()?.accessToken
+
+  useEffect(() => {
+    !accessToken && navigate('/login', { replace: true })
+  },[])
+
+  return accessToken ? <Component /> : <></>
 }
 
-export default privatRoute
+export default PrivateRoute
