32da5d033d273ea5c2ea25d95fafa29aa7f95c3e
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-26 09:05:19 -0300

    test: ensure SurveyItem load survey data correctly

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -1,27 +1,33 @@
 import { SurveyModel } from '../models'
 import faker from '@faker-js/faker'
 
-export const mockSurveyListModel = (): SurveyModel[] => {
+export const mockSurveyModel = (): SurveyModel => {
   const imageDimension = 200
+  return {
+    id: faker.datatype.uuid(),
+    question: faker.random.words(10),
+    answers: [
+      {
+        image: faker.image.food(imageDimension,imageDimension),
+        answer: faker.random.words(4)
+      },
+      {
+        answer: faker.random.words(5)
+      },
+      {
+        image: faker.image.cats(imageDimension,imageDimension),
+        answer: faker.animal.cat()
+      }
+    ],
+    date: faker.date.recent(),
+    didAnswer: (Math.random() > 0.5)
+  }
+}
+
+export const mockSurveyListModel = (): SurveyModel[] => {
   return [
-    {
-      id: faker.datatype.uuid(),
-      question: faker.random.words(10),
-      answers: [
-        {
-          image: faker.image.food(imageDimension,imageDimension),
-          answer: faker.random.words(4)
-        },
-        {
-          answer: faker.random.words(5)
-        },
-        {
-          image: faker.image.cats(imageDimension,imageDimension),
-          answer: faker.animal.cat()
-        }
-      ],
-      date: faker.date.recent(),
-      didAnswer: (Math.random() > 0.5)
-    }
+    mockSurveyModel(),
+    mockSurveyModel(),
+    mockSurveyModel()
   ]
 }
