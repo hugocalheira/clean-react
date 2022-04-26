ae924cf6b3ae9b6a30ec884d87c4af88eab6f158
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-26 15:19:52 -0300

    feat: add SurveyList composition

---
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -0,0 +1,8 @@
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
+import { RemoteLoadSurveyList } from '@/data/usecases'
+import { LoadSurveyList } from '@/domain/usecases'
+
+export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
+  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+}
