8ca50b83455f0ee988866a2bb96ee321204dc166
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-26 09:05:13 -0300

    feat: ensure SurveyItem load survey data correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -1,18 +1,24 @@
-import { Icon, IconName } from '@/presentation/components'
 import React from 'react'
+import { SurveyModel } from '@/domain/models'
+import { Icon, IconName } from '@/presentation/components'
 import Styles from './survey-item-styles.scss'
 
-const SurveyItem: React.FC = () => {
+type Props = {
+  survey: SurveyModel
+}
+
+const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
+  const { question, date } = survey
   return (
     <li className={Styles.surveyItemWrap}>
         <div className={Styles.surveyContent}>
             <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
             <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2022</span>
+                <span data-testid='day' className={Styles.day}>{date.getDate()}</span>
+                <span data-testid='month' className={Styles.month}>{date.toLocaleDateString('pt-BR', { month: 'short' }).replace('.','')}</span>
+                <span data-testid='year' className={Styles.year}>{date.getFullYear()}</span>
             </time>
-            <p>Qual o seu framework web favorito?</p>
+            <p data-testid='question'>{question}</p>
         </div>
         <footer>Ver Resultado</footer>
     </li>
