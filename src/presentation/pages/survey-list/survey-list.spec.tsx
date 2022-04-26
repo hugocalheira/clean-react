4d64ff0c94b4b8797df78c73f2c5e03dac309fcd
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-26 11:56:00 -0300

    test: ensure SurveyList renders SurveyItems on success

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import SurveyList from './survey-list'
+import { render, screen, waitFor } from '@testing-library/react'
 import { LoadSurveyList } from '@/domain/usecases'
 import { SurveyModel } from '@/domain/models'
+import { mockSurveyListModel } from '@/domain/test'
+import SurveyList from './survey-list'
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy
@@ -10,9 +11,10 @@ type SutTypes = {
 
 class LoadSurveyListSpy implements LoadSurveyList {
     callsCount = 0
+    surveys = mockSurveyListModel()
     async loadAll (): Promise<SurveyModel[]> {
       this.callsCount++
-      return []
+      return this.surveys
     };
 }
 
@@ -25,15 +27,26 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SurveyList Component', () => {
-  test('Should present 4 empty items on start', () => {
+  test('Should present 4 empty items on start', async () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
-    expect(surveyList.querySelectorAll('li').length).toBe(4)
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li')).toHaveLength(4)
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
 
-  test('Should call LoadSurveyList', () => {
+  test('Should call LoadSurveyList', async () => {
     const { loadSurveyListSpy } = makeSut()
     expect(loadSurveyListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  test('Should render SurveyItems on success', async () => {
+    const { loadSurveyListSpy } = makeSut()
+    const { surveys } = loadSurveyListSpy
+    const surveyList = screen.queryByTestId('survey-list')
+    // await waitFor(() => surveyList)
+    await screen.findByText(surveys[0].question)
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(surveys.length)
   })
 })
