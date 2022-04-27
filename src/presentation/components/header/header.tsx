1ba0dd8cbf552eb7390b743edb602edc50a4e9cb
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-27 15:32:24 -0300

    feat: ensure Header Component call SetCurrentAccount with null

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,26 @@
+import React, { memo, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Logo } from '@/presentation/components'
-import React, { memo } from 'react'
+import { ApiContext } from '@/presentation/contexts'
 import Styles from './header-styles.scss'
 
 const Header: React.FC = () => {
+  const { setCurrentAccount } = useContext(ApiContext)
+  const navigate = useNavigate()
+
+  const logout = (e: React.MouseEvent): void => {
+    e.preventDefault()
+    setCurrentAccount(null)
+    navigate('/login', { replace: true })
+  }
+
   return (
     <header className={Styles.headerWrap}>
         <div className={Styles.headerContent}>
             <Logo />
             <div className={Styles.logoutWrap}>
                 <span>Hugo</span>
-                <a href="#">sair</a>
+                <a data-testid='logout' onClick={logout} href="#">sair</a>
             </div>
         </div>
     </header>
