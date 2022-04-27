22147ba4930db73f675cfef17fbb9d57bae3cc10
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-27 15:39:12 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { fireEvent, render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { ApiContext } from '@/presentation/contexts'
 import Header from "./header"
 import 'jest-localstorage-mock'
 import { BrowserRouter } from 'react-router-dom'
+import { AccountModel } from '@/domain/models'
 
 // pay attention to write it at the top level of your file
 const mockedUsedNavigate = jest.fn()
@@ -13,17 +14,28 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate
 }))
 
+type SutTypes = {
+  setCurrentAccountMock: (accont: AccountModel) => void
+}
+
+const makeSut = (): SutTypes => {
+  const setCurrentAccountMock = jest.fn()
+  const sut = render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </ApiContext.Provider>
+  )
+  return {
+    setCurrentAccountMock
+  }
+}
+
 describe('Header Component', () => {
   test('Should call SetCurrentAccount with null', () => {
-    const setCurrentAccountMock = jest.fn()
-    const sut = render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </ApiContext.Provider>
-    )
-    fireEvent.click(sut.getByTestId('logout'))
+    const {setCurrentAccountMock} = makeSut()
+    fireEvent.click(screen.getByTestId('logout'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(null)
     expect(mockedUsedNavigate).toHaveBeenCalledWith('/login', { replace: true })
   })
