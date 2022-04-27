55a900d25de2b4534b358925b8b45b55e4bea80b
Hugo C. Durães <hugocalheira@gmail.com>
2022-04-27 13:20:06 -0300

    fix: ensure CompareFieldsValidation test never receive a invalid object to compare

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -7,7 +7,7 @@ const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation
 describe('CompareFieldsValidation', () => {
   test('Shoud return error if compare is invalid', () => {
     const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const fieldToCompare = faker.database.collation()
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
       [field]: faker.random.words(3),
@@ -18,7 +18,7 @@ describe('CompareFieldsValidation', () => {
 
   test('Shoud return falsy if compare is valid', () => {
     const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const fieldToCompare = faker.database.collation()
     const value = faker.random.words()
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({ [field]: value, [fieldToCompare]: value })
